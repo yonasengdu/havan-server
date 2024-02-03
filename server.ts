91401fd76a057712e64e46a443774aa03fe8a9f4
yohannesjoya <yohannesdestagebru10@gmail.com>
2024-02-03 22:11:05 +0300

    v1

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import {v2 as cloudinary} from "cloudinary";
 import http from "http";
 import connectDB from "./utils/db";
-import { initSocketServer } from "./socketServer";
+// import { initSocketServer } from "./socketServer";
 import { app } from "./app";
 require("dotenv").config();
 const server = http.createServer(app);

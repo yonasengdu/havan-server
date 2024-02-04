b19cadc9b6b853ff0017bc586b93d6b01ebc888c
yohannesjoya <yohannesdestagebru10@gmail.com>
2024-02-04 11:19:16 +0300

    added client origin

--- app.ts
+++ app.ts
@@ -25,6 +25,13 @@ app.use(
     credentials: true,
   })
 );
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 // api requests limit
 const limiter = rateLimit({

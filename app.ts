711de231716d144fd20d51465c9afeb43e0b4590
yohannesjoya <yohannesdestagebru10@gmail.com>
2024-02-04 11:24:23 +0300

    removed corss allow from all resources

--- app.ts
+++ app.ts
@@ -25,13 +25,6 @@ app.use(
     credentials: true,
   })
 );
-// Enable CORS for all routes
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 // api requests limit
 const limiter = rateLimit({
